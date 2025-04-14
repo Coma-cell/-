1f0bff8943d5c956305cf30cc97bd95c5c0f6a03
0x00er <saager2024@gmail.com>
2025-04-14 16:57:23 +0530

    reset on flip

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends VehicleBody3D
 
-
 @export var next_level = ""
 
 @onready var hit = $Hit
@@ -8,11 +7,32 @@ extends VehicleBody3D
 @onready var end = $CanvasLayer/End
 
 var finished = false
+var flip_time := 0.0
+var time_since_start := 0.0
+
+const FLIP_THRESHOLD := -0.7
+const FLIP_DURATION := 1.5  # seconds upside down
+const GRACE_PERIOD := 2.0   # seconds after level load
 
 
-func _physics_process(_delta):
+func _ready():
+	end.hide()
+
+
+func _physics_process(delta):
+	time_since_start += delta
 	wind.volume_db = clamp(linear_velocity.length() - 30, -80, 5)
-	
+
+	# Skip flip check during grace period or if already finished
+	if time_since_start > GRACE_PERIOD and not finished:
+		var up_dir = transform.basis.y
+		if up_dir.dot(Vector3.UP) < FLIP_THRESHOLD:
+			flip_time += delta
+			if flip_time >= FLIP_DURATION:
+				restart()
+		else:
+			flip_time = 0.0
+
 	if Input.is_key_pressed(KEY_R):
 		restart()
 
